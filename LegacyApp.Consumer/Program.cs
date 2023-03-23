d63a99d5c96dcbf491aec4683d8760e820e695d0
solar19 <karimi.marzieh@gmail.com>
2023-03-23 11:32:59 +0430

    first commit

---
+++ LegacyApp.Consumer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LegacyApp.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddUser(args);
+        }
+
+        public static void AddUser(string[] args)
+        {
+            // DO NOT CHANGE THIS FILE AT ALL
+            
+            var userService = new UserService();
+            var addResult = userService.AddUser("Pooya", "Alamirpour", "P.Alamirpour@gmail.com", new DateTime(1986, 10, 20), 4);
+            Console.WriteLine("Adding Pooya Alamirpour was " + (addResult ? "successful" : "unsuccessful"));
+        }
+    }
+}
